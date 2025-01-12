79ef3665aaaaad77ae62ee88b80eb7f49c2270a2
JamieWatton32 <jamiewatton199559955@gmail.com>
2025-01-12 16:39:12 -0400

    Working on database

--- Backend/src/classes/ClassroomsDAO.java
+++ Backend/src/classes/ClassroomsDAO.java
@@ -4,18 +4,33 @@ import java.sql.SQLException;
 
 public class ClassroomsDAO {
     // Inserts new row into Classrooms table
-    public void insertClassrooms(int id, String name, double capacity){
-        String sql = "INSERT INTO classrooms(id, name, capacity) VALUES(?, ?, ?)";
+    public static void insertClassrooms(int id, String complete){
+        String sql = "INSERT INTO classrooms(id, name) VALUES(?, ?)";
 
         try(var conn = DatabaseConnection.getConnection()){
             var pstmt = conn.prepareStatement(sql);
             pstmt.setInt(1, id);
-            pstmt.setString(2, name);
-            pstmt.setDouble(3, capacity);
+            pstmt.setString(2, complete);
+
             pstmt.executeUpdate();
             System.out.println("Inserted into classrooms.");
         }catch (SQLException e) {
             System.err.println("Error inserting into classroom table");
+        }
+    }
+    public static void getClassrooms() {
+        String sql = "SELECT * FROM classrooms";
+
+        try (var conn = DatabaseConnection.getConnection();
+             var stmt = conn.createStatement();
+             var rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                System.out.println(
+                        "ID: " + rs.getInt("id") +
+                                ", Name: " + rs.getString("complete"));
+            }
+        } catch (SQLException e) {
+            System.err.println("Error retrieving warehouses: " + e.getMessage());
         }
     }
 
--- Backend/src/net/sqlite/Main.java
+++ Backend/src/net/sqlite/Main.java
@@ -1,5 +1,8 @@
 package net.sqlite;
 
+import classes.ClassroomsDAO;
+import classes.DatabaseManager;
+
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,13 +13,19 @@ public class Main {
 
 
     public static void main(String[] args) {
+
+
+
         try (Connection connection = DriverManager.getConnection(path)) {
             if (connection != null) {
-                System.out.println("Connected to the database!");
+                DatabaseManager.setupDatabase();
+                System.out.println("Database setup complete.");
+                ClassroomsDAO.insertClassrooms(1,"Yes");
             }
         } catch (SQLException e) {
             System.err.println("Connection failed: " + e.getMessage());
         }
+
 
     }
 }
