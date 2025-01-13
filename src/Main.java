ed2ae51a4b6b5c80fec4c097012bfc4b72c59fe2
JamieWatton32 <jamiewatton199559955@gmail.com>
2025-01-13 19:23:31 -0400

    removed some stuff

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,8 @@
 import javax.swing.*;
-
-import BackEnd.DataApi;
-import BackEnd.DatabaseConnector;
 import FrontEnd.*;
-
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        try(var conn = DatabaseConnector.getConnection()){
-
-            System.out.println(DataApi.getClassrooms());
-            DatabaseConnector.closeConnection();
-        }catch(SQLException e){
-            System.err.println("Error: " + e);
-        }
-        JFrame ProgramWindow = new ProgramWindow();
+        JFrame _ = new ProgramWindow();
     }
 }
