05eb7342fd0167657b73c1fba34e776ea92c0708
shawarmagod <dariuskingfitess@gmail.com>
2025-01-14 16:50:43 -0400

    logic written to check for reports based on monthly & quarterly timelines. currently untested

--- src/FrontEnd/OutstandingPage.java
+++ src/FrontEnd/OutstandingPage.java
@@ -3,24 +3,34 @@ import BackEnd.DataApi;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.Objects;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
 
 public class OutstandingPage {
     private JPanel panel;
     private String[][] roomList = RoomList.getRoomList();
 
-    private ArrayList<String> rooms = new ArrayList<>();
+    private LocalDate currentDate;
+    private int currentQuarter;
+
+    private HashMap<String,String> rooms = new HashMap<>();
     private ArrayList<String> outstandingRooms = new ArrayList<>();
 
 
     public OutstandingPage() {
+        // Set current date & quarter
+        currentDate = LocalDate.now();
+        currentQuarter = findQuarter(currentDate);
+
+        // GUI
         panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)); // Stack components vertically
         panel.setBackground(Color.decode("#004477"));
 
-        findOutstanding(roomList);
+        findOutstanding(roomList); // Create a list of outstanding rooms
 
+        // Create a report on screen for each outstanding room
         for (String roomNumber : outstandingRooms) {
             JPanel report = new Report(roomNumber).createOutstanding();
             panel.add(report);
@@ -30,43 +40,121 @@ public class OutstandingPage {
 
     }
 
-    public void findOutstanding(String[][] roomList){
-        for (String[] i : roomList){
+    public void findOutstanding(String[][] roomList) {
+        for (String[] room : roomList){
             boolean flag = false;
 
-            if (Objects.equals(i[1], "office")) {
-                continue;
-            } else if (Objects.equals(i[1], "classroom")) {
-                rooms = DataApi.getClassrooms();
-
-                for (String room : rooms) {
-
-                    if (Objects.equals(i[0], room)){
-                        flag = true;
-                        break;
-                    }
+            switch (room[1]) {
+                case "office" -> {
+                    rooms = DataApi.getRoomDates("Offices");
                 }
-
-                if (flag == false) {
-                    outstandingRooms.add(i[0]);
+                case "classroom" -> {
+                    rooms = DataApi.getRoomDates("Classrooms");
+                    monthlyCheck(room[0], rooms, currentDate);
                 }
-            } else if (Objects.equals(i[1], "shop")) {
-                continue;
-            } else if (Objects.equals(i[1], "laboratory")) {
-                continue;
-            } else if (Objects.equals(i[1], "maintenance")) {
-                continue;
-            } else if (Objects.equals(i[1], "culinary")) {
-                continue;
-            } else {
-                //placeholder for error handling
-                continue;
+                case "shop" -> {
+                    rooms = DataApi.getRoomDates("Shops");
+                }
+                case "laboratory" -> {
+                    rooms = DataApi.getRoomDates("Laboratory");
+                }
+                case "maintenance" -> {
+                    rooms = DataApi.getRoomDates("Maintenance");
+                }
+                case "culinary" -> {
+                    rooms = DataApi.getRoomDates("Culinary");
+                }
+                case null, default -> {
+                    //placeholder for error handling
+                    continue;
+                }
             }
         }
     }
 
     public JPanel getPanel() {
         return panel;
+    }
+
+    public int findQuarter(LocalDate currentDate) {
+        int quarter = 0;
+        int month = currentDate.getMonthValue();
+
+        if (month < 4) {
+            quarter = 1;
+        } else if (month < 7) {
+            quarter = 2;
+        } else if (month < 10) {
+            quarter = 3;
+        } else {
+            quarter = 4;
+        }
+
+        return quarter;
+    }
+
+    public int findQuarter(int month) {
+        int quarter = 0;
+
+        if (month < 4) {
+            quarter = 1;
+        } else if (month < 7) {
+            quarter = 2;
+        } else if (month < 10) {
+            quarter = 3;
+        } else {
+            quarter = 4;
+        }
+
+        return quarter;
+    }
+
+    public void monthlyCheck (String roomNumber, HashMap<String,String> reports, LocalDate currentDate) {
+        boolean flag = false;
+
+        for (Map.Entry<String,String> report : reports.entrySet()) {
+            String reportRoomNumber = report.getKey();
+            int[] reportDate = Arrays.stream(report.getValue().replaceFirst("^(\\d{1,2})/(\\d{1,2})/(\\d{4}).*", "$1/$3").split("/"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+
+            if ((Objects.equals(roomNumber, reportRoomNumber)) && // if room numbers match
+                    (reportDate[0] == currentDate.getMonthValue()) && // & months match
+                    (reportDate[1] == currentDate.getYear())) { // & years match
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag == false) {
+            outstandingRooms.add(roomNumber);
+        }
+    }
+
+    public void quarterlyCheck (String roomNumber, HashMap<String,String> reports, LocalDate currentDate, int currentQuarter){
+        boolean flag = false;
+
+        for (Map.Entry<String,String> report : reports.entrySet()) {
+            String reportRoomNumber = report.getKey();
+            int[] reportDate = Arrays.stream(report.getValue().replaceFirst("^(\\d{1,2})/(\\d{1,2})/(\\d{4}).*", "$1/$3").split("/"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            int reportQuarter = findQuarter(reportDate[0]);
+
+            if ((Objects.equals(roomNumber, reportRoomNumber)) && // if room numbers match
+                    (reportQuarter == currentQuarter) && // & months match
+                    (reportDate[1] == currentDate.getYear())) { // & years match
+                flag = true;
+                break;
+            }
+        }
+
+
+
+        if (flag == false) {
+            outstandingRooms.add(roomNumber);
+        }
     }
 
 }
