26c192bdf420cf898db985fddc56c170d7aa30a4
shawarmagod <dariuskingfitess@gmail.com>
2025-01-17 13:18:44 -0400

    Checkpoint before overhaul:
    might be able to reduce to a single page and able to click into monthly entries

--- src/FrontEnd/Filters.java
+++ src/FrontEnd/Filters.java
@@ -23,4 +23,24 @@ public class Filters {
         }
 
     }
+
+    public static class ReportFilters {
+        JPanel filterContainer;
+
+        public ReportFilters() {
+            filterContainer = new JPanel();
+
+            // Monthly View Selector
+            JComboBox<String> monthDropDown = new JComboBox<>(new String[]{
+                    "Year to Date", "September", "October", "November", "December", "January",
+                    "February", "March", "April", "May", "June", "July", "August"
+            });
+            filterContainer.add(monthDropDown);
+        }
+
+        public JPanel getPanel() {
+            return filterContainer;
+        }
+
+    }
 }
